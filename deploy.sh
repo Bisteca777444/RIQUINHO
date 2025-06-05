e9dc7533d468820c0556109ec0f92f6b41b8c764
Bisteca777444 <almeidadiasjoseguilherme432@gmail.com>
2025-06-05 18:33:11 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 FROM php:8.2-fpm
 
 RUN apt-get update && apt-get install -y \
-    git zip unzip libzip-dev libpng-dev libonig-dev libxml2-dev \
+    git zip unzip libzip-dev libpng-dev libonig-dev libxml2-dev nodejs npm \
     && docker-php-ext-install pdo_mysql mbstring zip exif pcntl bcmath gd
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 WORKDIR /var/www/html
 
 COPY . .
 
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
 RUN composer install --no-dev --optimize-autoloader
+
+RUN npm install && npm run build
 
 EXPOSE 9000
 
